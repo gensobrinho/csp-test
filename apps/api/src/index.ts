7458f5c9129130d753954fda3539373c0345f7a2
Gabriel Estevão <gensobrinho@hotmail.com>
2026-09-10 19:42:19 -0300

    feat: setup express

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
