dc3663738f465033fe2ce4c9cb4e4d68678d30e2
Gabriel Estevão <gensobrinho@hotmail.com>
2026-09-13 11:03:24 -0300

    feat: create i18n

---
+++ apps/web/src/shared/i18n/index.ts
@@ -0,0 +1,5 @@
+import PT_BR from './ptBr';
+
+const TEXTS = PT_BR;
+
+export default TEXTS;
---
+++ apps/web/src/shared/i18n/ptBr.ts
@@ -0,0 +1,13 @@
+const PT_BR = {
+  auth: {
+    login: 'Acesse sua conta',
+    username: 'Usuário',
+    password: 'Senha',
+    access: 'Acessar',
+    usernamePlaceholder: 'Digite seu usuário',
+    passwordPlaceholder: 'Digite sua senha',
+    userNotFound: 'Usuário não encontrado',
+  },
+} as const;
+
+export default PT_BR;
