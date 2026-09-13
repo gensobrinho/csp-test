aef94f7dee78372b026163360678104c62f92c00
Gabriel Estevão <gensobrinho@hotmail.com>
2026-09-13 11:59:45 -0300

    setup testing library

--- apps/web/src/shared/utils/testSetup.tsx
+++ apps/web/src/shared/utils/testSetup.tsx
@@ -1,7 +1,14 @@
-// import { ThemeProvider } from '@emotion/react';
-// import { RenderOptions, render } from '@testing-library/react';
-// import { theme } from '../theme';
+import { ThemeProvider } from '@emotion/react';
+import { RenderOptions, render } from '@testing-library/react';
+import { theme } from '../theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactElement } from 'react';
 
-// export const renderWithProviders = (Component: React.ReactElement, options?: RenderOptions) => {
-//   return render(<ThemeProvider theme={theme}>{Component}</ThemeProvider>, options);
-// };
+export const renderWithProviders = (component: ReactElement, options?: RenderOptions) => {
+    return render(
+    <QueryClientProvider client={new QueryClient()}>
+      <ThemeProvider theme={theme}>{component}</ThemeProvider>
+    </QueryClientProvider>,
+    options
+  );
+};
