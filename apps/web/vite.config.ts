10ed4200f875f67bc62df4438f2169270d9805d3
Gabriel Estevão <gensobrinho@hotmail.com>
2026-09-13 10:39:24 -0300

    adjust vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@shared/utils': path.resolve(__dirname, './src/shared/utils'),
-      '@shared/types': path.resolve(__dirname, './src/shared/types'),
-      '@shared/enums': path.resolve(__dirname, './src/shared/enums'),
-      '@shared/store': path.resolve(__dirname, './src/shared/store'),
+      '@': fileURLToPath(new URL('.', import.meta.url)),
+      '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
+      '@shared/utils': fileURLToPath(new URL('./src/shared/utils', import.meta.url)),
+      '@shared/types': fileURLToPath(new URL('./src/shared/types', import.meta.url)),
+      '@shared/enums': fileURLToPath(new URL('./src/shared/enums', import.meta.url)),
+      '@shared/store': fileURLToPath(new URL('./src/shared/store', import.meta.url)),
     }
   }
 });
