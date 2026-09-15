29f0cc1257e1bdb7b180f6924e52dd3e861db415
Gabriel Estevão <gensobrinho@hotmail.com>
2026-09-15 00:11:42 -0300

    feat: remove demo feature

--- apps/web/src/features/Demo/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as demoSlice } from './sliceStore/demoSliceStore';
-export type {
-  IDemoInitialState,
-  IDemoActions,
-} from './sliceStore/demoSliceStore';
--- apps/web/src/features/Demo/types/TDemo.ts
+++
@@ -1,4 +0,0 @@
-export type TDemo = {
-  id?: string;
-  name?: string;
-};
