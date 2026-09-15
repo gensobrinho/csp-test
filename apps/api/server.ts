64bbd4a5cf1efcd3b465f898e5f50468aacb4a64
Gabriel Estevão <gensobrinho@hotmail.com>
2026-09-15 16:23:47 -0300

    feat: adjust server.ts to deploy

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -4,7 +4,7 @@ import { createApp } from './src/index.js';
 
 const { app, container } = createApp();
 
-const server = app.listen(env.port, () => {
+const server = app.listen(env.port, '0.0.0.0', () => {
   console.log(`API running at http://localhost:${env.port}`);
 });
 
